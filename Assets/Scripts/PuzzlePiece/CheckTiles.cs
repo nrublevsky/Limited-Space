b5d46d62f9ebd7c106c2e3a8555c0cf56834f326
nrublevsky <143497170+nrublevskiy@users.noreply.github.com>
2023-09-30 18:50:15 +0200

    Puzzle piece \ tile detector

---
+++ Assets/Scripts/PuzzlePiece/CheckTiles.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CheckTiles : MonoBehaviour
+{
+    public TileBehavior currentTile;
+    public PuzzlePieceBehavior piece;
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Tile"))
+        {
+            currentTile = collision.gameObject.GetComponent<TileBehavior>();
+            piece.interactedTiles.Add(currentTile);
+        }
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Tile"))
+        {
+            currentTile = collision.gameObject.GetComponent<TileBehavior>();
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Tile"))
+        {
+            currentTile = collision.gameObject.GetComponent<TileBehavior>();
+            piece.interactedTiles.Remove(currentTile);
+        }
+    }
+}
