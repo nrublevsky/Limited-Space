8fb4dbf873ea8cc597587a4a8764161848937505
CrazyDuck8 <remixman115@gmail.com>
2023-09-30 16:46:26 +0300

    MousDragAdded

---
+++ Assets/Scripts/LevelTile/Draggable.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Draggable : MonoBehaviour
+{
+    Vector2 mousePositionOffset;
+
+    private Vector2 GetMouseWorldPosition()
+    {
+
+        return Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    }
+    // Start is called before the first frame update
+    private void OnMouseDown()
+    {
+        mousePositionOffset= GetMouseWorldPosition();
+    }
+    private void OnMouseDrag()
+    {
+        transform.position = GetMouseWorldPosition();
+    }
+}
